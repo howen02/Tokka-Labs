1fe46f9ee6fc64b156d5486a4ca9b98f410a8511
howen02 <wenhyap02@gmail.com>
2024-10-18 22:25:18 +0800

    Create helper function to build request to etherscan API and return typed response

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -2,5 +2,6 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const UNISWAP_ETH_USDC_CONTRACT_ADDRESS = "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640"
+export const UNISWAP_POLL_ADDRESS = "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640"
 export const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY_1
+export const ETHERSCAN_URL = 'https://api.etherscan.io/api'
---
+++ backend/src/utils.ts
@@ -0,0 +1,21 @@
+import {ETHERSCAN_API_KEY, ETHERSCAN_URL} from "./constants";
+
+export const buildUrl = (params: URLSearchParams) => {
+    if (ETHERSCAN_API_KEY) params.append('apikey', ETHERSCAN_API_KEY);
+
+    return `${ETHERSCAN_URL}?${params.toString()}`;
+}
+
+type GenericResponse<T> = {
+    status: string;
+    message: string;
+    result: T;
+}
+
+export const buildRequestAndFetch = <T>(params: URLSearchParams): Promise<T> =>
+    Promise.resolve(params)
+    .then(buildUrl)
+    .then(fetch)
+    .then(res => res.json() as unknown as GenericResponse<T>)
+    .then(typedResponse => typedResponse.result)
+    .catch(err => Promise.reject(err));
