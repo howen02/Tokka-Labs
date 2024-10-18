71001e1dc66e36b89b6d23e2dd1b64405e3d4bfc
howen02 <wenhyap02@gmail.com>
2024-10-18 21:12:51 +0800

    Create endpoint to search for transaction in db via hash

--- backend/src/controllers/fetchTransactionController.ts
+++ backend/src/controllers/fetchTransactionController.ts
@@ -1,12 +1,15 @@
 import db from "../db/db";
+import {Transaction} from "../types";
 
-const fetchTransactionFee = (hash: string) => {
+export const fetchTransaction = (hash: string) =>
+    Promise.resolve(hash)
+        .then(findTransactionInDb)
+        .then(tx =>
+            tx
+                ? {status: 200, body: {message: 'Transaction found', data: JSON.stringify(tx)}}
+                : {status: 404, body: {message: 'Transaction not found'}})
 
-}
-
-const findTransactionInDb = (hash: string) =>
+const findTransactionInDb = (hash: string): Transaction =>
     db
-        .query("SELECT * FROM transactions WHERE hash = ?")
-        .get(hash);
-
-const fetchTransactionFromEtherscan = (hash: string) => null
+    .query("SELECT * FROM transactions WHERE hash = $hash")
+    .get({$hash: hash}) as Transaction
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,11 @@
 import { Elysia } from "elysia";
 import { pollTransactions} from "./services/pollTransactions";
+import {fetchTransaction} from "./controllers/fetchTransactionController";
 
 const app =
     new Elysia()
         .get("/transctions", () => "Hello, Elysia!")
-        .get("/tx/:hash", () => "Hello, Elysia!")
+        .get("/tx/:hash", ({ params: { hash } }) => fetchTransaction(hash))
         .listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
