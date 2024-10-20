dac65572d8ae7fe68bcb50c634d506e82a97390e
howen02 <wenhyap02@gmail.com>
2024-10-20 23:01:58 +0800

    Separate query functions

--- frontend/src/TransactionsPage.tsx
+++ frontend/src/TransactionsPage.tsx
@@ -10,40 +10,35 @@ import { DateRange } from 'react-day-picker'
 import { X } from 'lucide-react'
 import EthPriceCard from '@/components/EthPrice.tsx'
 
+const fetchTransactions = (page: number, pageSize: number) =>
+	Promise.resolve(new URLSearchParams({ page: page.toString(), pageSize: pageSize.toString() }))
+		.then(params => fetch(`${BACKEND_URL}/transactions?${params}`))
+		.then(response => response.json())
+		.then((data: Response<Transaction[]>) => data.body.data)
+
+const fetchTransactionByHash = (hash: string) =>
+	fetch(`${BACKEND_URL}/transaction/${hash}`)
+		.then(response => response.json())
+		.then((data: Response<Transaction[]>) => data.body.data)
+
+const fetchTransactionsByTimeRange = (start: number, end: number, page: number, pageSize: number) =>
+	Promise.resolve(new URLSearchParams({
+		start: start.toString(),
+		end: end.toString(),
+		page: page.toString(),
+		pageSize: pageSize.toString()
+	}))
+		.then(params => fetch(`${BACKEND_URL}/transactions/range?${params}`))
+		.then(response => response.json())
+		.then((data: Response<Transaction[]>) => data.body.data)
+
+const processDate = (date: Date) => Math.floor(date.getTime() / 1000)
+
 function TransactionsPage() {
 	const [page, setPage] = useState(1)
 	const [pageSize, setPageSize] = useState(50)
 	const [transactionHash, setTransactionHash] = useState('')
 	const [dateRange, setDateRange] = useState<DateRange | undefined>()
-
-	const fetchTransactions = ({
-		queryKey
-	}: {
-		queryKey: [
-			string,
-			{
-				page: number
-				pageSize: number
-				hash?: string
-				start?: number
-				end?: number
-			}
-		]
-	}) => {
-		const [_key, { page, pageSize, hash, start, end }] = queryKey
-
-		let url = `${BACKEND_URL}/transactions?page=${page}&pageSize=${pageSize}`
-
-		if (hash) {
-			url = `${BACKEND_URL}/transaction/${hash}`
-		} else if (start !== undefined && end !== undefined) {
-			url = `${BACKEND_URL}/transactions/range?start=${start}&end=${end}&page=${page}&pageSize=${pageSize}`
-		}
-
-		return fetch(url)
-			.then(response => response.json())
-			.then((data: Response<Transaction[]>) => data.body.data)
-	}
 
 	const { data: transactions } = useQuery({
 		queryKey: [
@@ -51,23 +46,33 @@ function TransactionsPage() {
 			{
 				page,
 				pageSize,
-				hash: transactionHash || undefined,
-				start:
-					dateRange?.from ?
-						Math.floor(dateRange.from.getTime() / 1000)
-					:	undefined,
-				end:
-					dateRange?.to ? Math.floor(dateRange.to.getTime() / 1000) : undefined
+				hash: transactionHash.trim() || undefined,
+				start: dateRange?.from ? processDate(dateRange.from) : undefined,
+				end: dateRange?.to ? processDate(dateRange.to) : undefined
 			}
 		],
-		queryFn: fetchTransactions,
+		queryFn: ({ queryKey }: { queryKey: [string, {
+				page: number
+				pageSize: number
+				hash?: string
+				start?: number
+				end?: number
+			}] }) => {
+			const [_, { page, pageSize, hash, start, end }] = queryKey
+			switch (true) {
+				case !!hash:
+					return fetchTransactionByHash(hash)
+				case start !== undefined && end !== undefined:
+					return fetchTransactionsByTimeRange(start, end, page, pageSize)
+				default:
+					return fetchTransactions(page, pageSize)
+			}
+		},
 		staleTime: 10 * 60 * 1000,
 		refetchInterval: 1000
 	})
 
-	const handlePageChange = (newPage: number) => {
-		setPage(newPage)
-	}
+	const handlePageChange = (newPage: number) => setPage(newPage)
 
 	const handlePageSizeChange = (newPageSize: number) => {
 		setPageSize(newPageSize)
@@ -82,8 +87,7 @@ function TransactionsPage() {
 		setDateRange(undefined)
 	}
 
-	const hasNextPage =
-		(transactions && transactions.length === pageSize) ?? false
+	const hasNextPage = (transactions && transactions.length === pageSize) ?? false
 
 	return (
 		<div className="h-screen flex justify-center items-center">
@@ -93,13 +97,13 @@ function TransactionsPage() {
 						type="text"
 						placeholder="Search hash"
 						value={transactionHash}
-						onChange={e => setTransactionHash(e.target.value)}
+						onChange={e => setTransactionHash(e.target.value.trim())}
 					/>
 					<DateRangePicker value={dateRange} onChange={handleDateRangeChange} />
 					<Button
 						className="w-2"
 						variant="destructive"
-						onClick={() => resetFilters()}
+						onClick={resetFilters}
 					>
 						<X />
 					</Button>
