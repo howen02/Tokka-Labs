9d2b92a721f98af26f078d64fffb9f849396b0ee
howen02 <wenhyap02@gmail.com>
2024-10-21 02:33:11 +0800

    Remove leader architecture, all backend instances poll

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,16 +7,9 @@ import {
 import { getTransaction } from './handlers/handleFetchTransaction';
 import { pollTransactionsCron } from './middleware';
 import { SERVER_PORT } from './constants';
-import { createClient } from 'redis';
-
-const redisClient = createClient({
-	url: `redis://${process.env.REDIS_HOST}:6379`
-});
-const instanceId = `instance_${Math.random().toString(36).substring(2, 10)}`;
-const LEADER_KEY = 'leader';
-const LEADER_TTL = 60; // Longer TTL since we aren't renewing
 
 const app = new Elysia()
+	.use(pollTransactionsCron)
 	.use(cors({
 		origin: ['http://localhost:4173', 'http://frontend:4173', 'http://localhost:8080'],
 	}))
@@ -36,26 +29,5 @@ const app = new Elysia()
 					getTransactionsInTimeRange(start, end, Number(page), Number(pageSize))
 			)
 	)
-	.get('/transaction/:hash', ({ params: { hash } }) => getTransaction(hash));
-
-redisClient
-	.connect()
-	.then(() => {
-		console.log('Connected to Redis.');
-		return redisClient.set(LEADER_KEY, instanceId, { NX: true, EX: LEADER_TTL });
-	})
-	.then(result => {
-		result
-			? console.log('This instance is now the leader.')
-			: console.log('This instance is a follower.');
-
-		if (result) {
-			app.use(pollTransactionsCron);
-		}
-
-		return app.listen(SERVER_PORT);
-	})
-	.then(() =>
-		console.log(`Backend is running at ${app.server?.hostname}:${app.server?.port}`)
-	)
-	.catch(error => console.error('Error during setup:', error));
+	.get('/transaction/:hash', ({ params: { hash } }) => getTransaction(hash))
+	.listen(SERVER_PORT);
